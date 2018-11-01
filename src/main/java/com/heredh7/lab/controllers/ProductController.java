b0f441b8f30446fc9c2c6a60a9cc0d4602e36e43
dani <daniel.heredia@ext.privalia.com>
2018-10-31 17:53:40 -0700

    [31-10-2018] added individual product page with data from DB

--- src/main/java/com/heredh7/lab/controllers/ProductController.java
+++ src/main/java/com/heredh7/lab/controllers/ProductController.java
@@ -2,14 +2,14 @@ package com.heredh7.lab.controllers;
 
 import com.heredh7.lab.entities.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
@@ -26,35 +26,80 @@ public class ProductController {
     /**
      * View Single product
      */
-    @RequestMapping(value="/product{id}", method = RequestMethod.GET)
+    @RequestMapping("{id}/")
     public String product(Model model, @PathVariable Integer id){
-        model.addAttribute("productName","My Custom Page Title");
-        model.addAttribute("products",getProduct(id));
+        //model.addAttribute("productName","My Custom Page Title");
+
+        model.addAttribute("productId",id);
+        model.addAttribute("productName",getProduct(id).getName());
+        model.addAttribute("productDescription",getProduct(id).getDescription());
+        model.addAttribute("productPrice",getProduct(id).getPrice());
+        model.addAttribute("productImage",getProduct(id).getImage());
         return "views/product";
     }
+
+   /* @RequestMapping("/actualidad/{id}")
+    public String detalle(@PathVariable Long id, Model model) {
+        model.addAttribute("noticiaDetalle", this.noticiaService.get(id));
+        return actualidad;
+    }*/
 
     /**
      * Single Product
      * @param id product
      * @return
      */
-    public Product getProduct(Integer id){
+    public Product getProduct(Integer idProduct){
 
-        Product product1= new Product();
-        product1.setId(1);
+        Product product= new Product();
+        /*product1.setId(1);
         product1.setName("Detergente HOLA Ariel");
         product1.setDescription("Detergente más económico del mercado");
         product1.setPrice(2.36);
+        product1.setImage("/images/blablabla");*/
+
+        //System.out.println("HELLO WORLD: trying connection from ProductController");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/heredh7_sl?autoReconnect=true&useSSL=false","user_test_here7","password");
+            Statement stm = con.createStatement();
+            stm = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE); //I get it 2 variables to can to do UPDATES in the table
+
+
+            // Method 1 to move through results ( Lineal Method => with next() )
+            ResultSet result = stm.executeQuery("SELECT * FROM SL_PRODUCT WHERE id = " + idProduct);
+
+            while(result.next()){
+                if(result.wasNull()){
+                    System.out.println(" RESULT without expedient...");
+                } else{
+                    System.out.println(" Register PRODUCT -" + result.getInt("id") + "- it's Okay");
+                   // Product product = new Product();
+                    product.setId(result.getInt("id"));
+                    product.setName(result.getString("name"));
+                    product.setDescription(result.getString("description"));
+                    product.setImage(result.getString("image"));
+                    product.setPrice(result.getDouble("price"));
+                    //products.add(product);
+                }
+            }
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         //return "redirect/
-        return product1;
+        return product;
     }
 
 
-    private ArrayList<Product> createProducts(){
-        ArrayList<Product> products = new ArrayList<Product>();
+    private List<Product> createProducts(){
+        List<Product> products = new ArrayList<Product>();
 
-        System.out.println("HELLO WORLD: trying connection from ProductController");
+        //System.out.println("HELLO WORLD: trying connection from ProductController");
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/heredh7_sl?autoReconnect=true&useSSL=false","user_test_here7","password");
@@ -78,10 +123,6 @@ public class ProductController {
                     product.setPrice(result.getDouble("price"));
                     products.add(product);
                 }
-                /*System.out.println("=>  ID: "+result.getInt("id") + "; \t" +
-                        "USER:" + result.getString("username") +"; \t" +
-                        "START DATE: "+result.getDate("start_date"));*/
-
             }
 
 
@@ -90,21 +131,6 @@ public class ProductController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-       /* Product product1= new Product();
-        product1.setId(1);
-        product1.setName("Detergente Ariel");
-        product1.setDescription("Detergente más económico del mercado");
-        product1.setPrice(2.36);
-        products.add(product1);
-
-        Product product2 = new Product();
-        product2.setId(2);
-        product2.setName("Detergente Tide");
-        product2.setDescription("Detergente más concentrado del mercado");
-        product2.setPrice(3.13);
-        products.add(product2);*/
-
 
         return products;
     }
